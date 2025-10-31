f25beab4031392cad16c225252382b0891ebd190
Dosu333 <vladamir1865@gmail.com>
2025-10-31 13:51:35 +0100

    Refactor cost function calculations in LinearRegression for improved readability

--- linear_regression/lin_reg.py
+++ linear_regression/lin_reg.py
@@ -11,7 +11,7 @@ class LinearRegression:
     def _cost_function(self, X, y, w, b):
         m = len(y)
         predictions = X.dot(w) + b
-        cost = (1/(2*m)) * np.sum((predictions - y)**2)
+        cost = (1 / (2 * m)) * np.sum((predictions - y) ** 2)
         return cost
 
     def fit(self, X, y):
@@ -23,8 +23,8 @@ class LinearRegression:
             predictions = X.dot(self.w) + self.b
             prev_cost = new_cost
             new_cost = self._cost_function(X, y, self.w, self.b)
-            d_dw = (1/m) * np.sum((predictions - y) * X)
-            d_db = (1/m) * np.sum(predictions - y)
+            d_dw = (1 / m) * np.sum((predictions - y) * X)
+            d_db = (1 / m) * np.sum(predictions - y)
             self.w = self.w - (self.learning_rate * d_dw)
             self.b = self.b - (self.learning_rate * d_db)
 
